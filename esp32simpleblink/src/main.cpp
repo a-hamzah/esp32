44d74a2e434a5b1bc3bda6c4af05b56c37ca8743
UET-Boy <Spellbreaker907@gmail.com>
2022-12-22 12:28:04 +0500

    project added

--- esp32simpleblink/src/main.cpp
+++ esp32simpleblink/src/main.cpp
@@ -1,15 +1,18 @@
-//adding this because we are working in arduino development framework
+// adding this because we are working in arduino development framework
 #include <Arduino.h>
 
-void setup() {
-pinMode (2, OUTPUT);
-
+void setup()
+{
+    pinMode(2, OUTPUT);
+    Serial.begin(9600);
 }
 
-void loop() {
-digitalWrite(2, HIGH);
-delay(5000);
-digitalWrite(2, LOW);
-delay(5000);
-
+void loop()
+{
+    digitalWrite(2, HIGH);
+    Serial.println("Light is ON");
+    delay(5000);
+    digitalWrite(2, LOW);
+    Serial.println("Light is OFF");
+    delay(5000);
 }
